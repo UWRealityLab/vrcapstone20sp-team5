1d776d7b345559fe21bb815b51aa72ff1ac4cdc9
yuxuan-bill <luyxapp@163.com>
2020-04-30 15:11:50 -0700

    add unity project built by yuxuan

---
+++ Assets/Scripts/ScoreKeeping.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeping : MonoBehaviour
+{
+    
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(other.gameObject);
+        score += 1;
+        Debug.Log("Score: " + score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
