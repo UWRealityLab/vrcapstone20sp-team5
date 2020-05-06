545234f62edd55b0883fe2e9f84feedea5211bf9
Yating Han <hany39@cs.washington.edu>
2020-05-06 10:21:24 -0700

    modeified spwan manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ namespace PathCreation {
 
     #region Public Variables  
     public PlaneController PlaneScript;
-    public GameObject Ball;
+    public GameObject TrailAndBall;
     public DisplayTrailAndBall BallScript;
     public float SpawnFrequency = 10f;
     public Text ScanText;
@@ -56,45 +56,75 @@ namespace PathCreation {
     // first select a random plane
     // then randomly select one point on the plane to spawn the ball
     private void RandomSpawn2() {
-        float xmin = 10f, xmax = -10f, ymin = 10f, ymax = -10f, zmin = 10f, zmax = -10f;
-        int StartPlaneIndex = 0;
-        GameObject StartPlane = PlaneScript.planeCache[StartPlaneIndex];
+        // float xmin = 10f, xmax = -10f, ymin = 10f, ymax = -10f, zmin = 10f, zmax = -10f;
+        // int StartPlaneIndex = 0;
+        // GameObject StartPlane = PlaneScript.planeCache[StartPlaneIndex];
         
-        // choose a plane within the range
-        while (xmin > 4f || xmax < -4f || ymin > 0.3f ||  zmin > 4f || zmax < -4f) {
-            StartPlaneIndex = Random.Range(0, numPlane);
-            StartPlane = PlaneScript.planeCache[StartPlaneIndex];
-            xmin = StartPlane.transform.position.x - StartPlane.transform.localScale.x/2;
-            xmax = StartPlane.transform.position.x + StartPlane.transform.localScale.x/2;
-            ymin = StartPlane.transform.position.y - StartPlane.transform.localScale.y/2;
-            ymax = StartPlane.transform.position.y + StartPlane.transform.localScale.y/2;
-            zmin = StartPlane.transform.position.z - StartPlane.transform.localScale.z/2;
-            zmax = StartPlane.transform.position.z + StartPlane.transform.localScale.z/2;
-        }
+        // // choose a plane within the range
+        // while (xmin > 4f || xmax < -4f || ymin > 0.3f ||  zmin > 4f || zmax < -4f) {
+        //     StartPlaneIndex = Random.Range(0, numPlane);
+        //     StartPlane = PlaneScript.planeCache[StartPlaneIndex];
+        //     xmin = StartPlane.transform.position.x - StartPlane.transform.localScale.x/2;
+        //     xmax = StartPlane.transform.position.x + StartPlane.transform.localScale.x/2;
+        //     ymin = StartPlane.transform.position.y - StartPlane.transform.localScale.y/2;
+        //     ymax = StartPlane.transform.position.y + StartPlane.transform.localScale.y/2;
+        //     zmin = StartPlane.transform.position.z - StartPlane.transform.localScale.z/2;
+        //     zmax = StartPlane.transform.position.z + StartPlane.transform.localScale.z/2;
+        // }
 
-        // // make sure spawn point is not too high/low/far
-        // float StartX = Random.Range(Max(xmin, -4f), Min(4f, xmax));
-        // float StartY = Random.Range(ymin, Min(.5f, ymax));
-        // float StartZ = Random.Range(Max(zmin, -4f), Min(4f, zmax));
+        // // // make sure spawn point is not too high/low/far
+        // // float StartX = Random.Range(Max(xmin, -4f), Min(4f, xmax));
+        // // float StartY = Random.Range(ymin, Min(.5f, ymax));
+        // // float StartZ = Random.Range(Max(zmin, -4f), Min(4f, zmax));
 
-        // // adjust spawn location and rotation (normal vector of the plane)
-        // Vector3 normal = StartPlane.transform.rotation*new Vector3(0, 0, 1);
-        // Vector3 proj = Vector3.ProjectOnPlane(new Vector3(StartX, StartY, StartZ), normal);
+        // // // adjust spawn location and rotation (normal vector of the plane)
+        // // Vector3 normal = StartPlane.transform.rotation*new Vector3(0, 0, 1);
+        // // Vector3 proj = Vector3.ProjectOnPlane(new Vector3(StartX, StartY, StartZ), normal);
         
-        // BallScript.path.transform.position = proj;
-        // BallScript.path.transform.rotation = Quaternion.Euler(normal);
-        float width = Random.Range(-0.5f, 0.5f);
-        float height = Random.Range(-0.5f, 0.5f);
-        Vector3 right = StartPlane.transform.rotation*Vector3.right;
-        Vector3 location = StartPlane.transform.position + 
-            right*(StartPlane.transform.localScale.x * width);
+        // // BallScript.path.transform.position = proj;
+        // // BallScript.path.transform.rotation = Quaternion.Euler(normal);
+        // float width = Random.Range(-0.5f, 0.5f);
+        // float height = Random.Range(-0.5f, 0.5f);
+        // Vector3 right = StartPlane.transform.rotation*Vector3.right;
+        // Vector3 location = StartPlane.transform.position + 
+        //     right*(StartPlane.transform.localScale.x * width);
         
-        location = location + Vector3.up * (StartPlane.transform.localScale.y*height);
+        // location = location + Vector3.up * (StartPlane.transform.localScale.y*height);
 
-        BallScript.path.transform.position = location;
-        BallScript.path.transform.rotation = Quaternion.LookRotation(StartPlane.transform.rotation*Vector3.forward);
-        // spawn the train and ball, 
-        Instantiate(Ball);
+        // BallScript.path.transform.position = location;
+        // BallScript.path.transform.rotation = Quaternion.LookRotation(StartPlane.transform.rotation*Vector3.forward);
+        // // spawn the train and ball, 
+        // Instantiate(Ball);
+
+        GameObject planeStart = PlaneScript.planeCache[Random.Range(0, numPlane)];
+        GameObject planeEnd = planeStart;
+        while (planeEnd == planeStart) planeEnd = PlaneScript.planeCache[Random.Range(0, numPlane)];
+        DisplayTrailAndBall spawner = TrailAndBall.GetComponent<DisplayTrailAndBall>();
+
+        float xStart = Random.Range(planeStart.transform.position.x - planeStart.transform.localScale.x/2,
+            planeStart.transform.position.x + planeStart.transform.localScale.x/2);
+        float yStart = Random.Range(planeStart.transform.position.y - planeStart.transform.localScale.y/2,
+            planeStart.transform.position.y + planeStart.transform.localScale.y/2);
+        float zStart = Random.Range(planeStart.transform.position.z - planeStart.transform.localScale.z/2,
+            planeStart.transform.position.z + planeStart.transform.localScale.z/2);
+        Collider collStart = planeStart.GetComponent<Collider>();
+        Vector3 startLoc = collStart.ClosestPointOnBounds(new Vector3(xStart, yStart, zStart));
+
+        float xEnd = Random.Range(planeEnd.transform.position.x - planeEnd.transform.localScale.x/2,
+            planeEnd.transform.position.x + planeEnd.transform.localScale.x/2);
+        float yEnd = Random.Range(planeEnd.transform.position.y - planeEnd.transform.localScale.y/2,
+            planeEnd.transform.position.y + planeEnd.transform.localScale.y/2);
+        float zEnd = Random.Range(planeEnd.transform.position.z - planeEnd.transform.localScale.z/2,
+            planeEnd.transform.position.z + planeEnd.transform.localScale.z/2);
+        Collider collEnd = planeEnd.GetComponent<Collider>();
+        Vector3 endLoc = collEnd.ClosestPointOnBounds(new Vector3(xEnd, yEnd, zEnd));
+
+        spawner.start = startLoc;
+        spawner.end = endLoc;
+        spawner.middle = (startLoc + endLoc) / 2;
+        
+        Instantiate(TrailAndBall, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+        
         
     }
 
